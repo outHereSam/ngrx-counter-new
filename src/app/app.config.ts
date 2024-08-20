68cebd2adecb34af0600d8b060424fe6a9991e29
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2024-08-20 21:07:34 +0000

    Implements selector for count

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,16 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
+import { provideStore, provideState } from '@ngrx/store';
+import { countReducer } from './state/counter/counter.reducer';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore()]
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideState({
+      name: 'count',
+      reducer: countReducer,
+    }),
+  ],
 };
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { increment, decrement, reset } from '../state/counter/counter.actions';
+import { AsyncPipe } from '@angular/common';
+import { selectCount } from '../state/counter/counter.selectors';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
 })
 export class CounterComponent {
+  count$?: Observable<number>;
 
+  constructor(private store: Store<{ count: number }>) {
+    this.count$ = store.select(selectCount);
+    this.store
+      .select((state) => state.count)
+      .subscribe((count) => console.log(count));
+  }
+
+  increment() {
+    this.store.dispatch(increment());
+  }
+
+  decrement() {
+    this.store.dispatch(decrement());
+  }
+
+  reset() {
+    this.store.dispatch(reset());
+  }
 }
